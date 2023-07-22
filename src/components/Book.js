7b81063c6ae1a8a97017b751a6df1375d666af71
DevaWinner <winneraaniekan@gmail.com>
2023-07-22 22:23:35 +0100

    Add book as component and import

---
+++ src/components/Book.js
@@ -0,0 +1,13 @@
+const books = [
+  {
+    id: 1, title: 'Book-1', author: 'Author-1', category: 'Category-1',
+  },
+  {
+    id: 2, title: 'Book-2', author: 'Author-2', category: 'Category-2',
+  },
+  {
+    id: 3, title: 'Book-3', author: 'Author-3', category: 'Category-3',
+  },
+];
+
+export default books;
