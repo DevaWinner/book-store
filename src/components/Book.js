c2bcc5a05cc1d54fad9e4cba7b0c459687a653b6
DevaWinner <winneraaniekan@gmail.com>
2023-07-28 17:17:19 +0100

    Update Book component with Stats and Chapter

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Chapter from './Chapter';
+import Stats from './Stats';
 
 function Book({ book, deleteBook }) {
   return (
     <div>
       <li>
         <div>
-          <p>Title:</p>
-          {book.title}
+          <div>
+            <div>
+              {book.category}
+            </div>
+            <div>
+              {book.title}
+            </div>
+            <div>
+              {book.author}
+            </div>
+          </div>
+          <div>
+            <button type="button">
+              Comments
+            </button>
+            <button type="button" onClick={() => deleteBook(book.id)}>
+              Remove
+            </button>
+            <button type="button">
+              Edit
+            </button>
+          </div>
         </div>
-        <div>
-          <p>Author:</p>
-          {book.author}
-        </div>
-        <div>
-          <p>Category:</p>
-          {book.category}
-        </div>
-        <button type="button" onClick={() => deleteBook(book.id)}>
-          Delete Book
-        </button>
+        <Stats />
+        <Chapter />
       </li>
     </div>
   );
