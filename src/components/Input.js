e98c3d9f7104780c407f885f281857cc4b2b47cf
DevaWinner <winneraaniekan@gmail.com>
2023-07-29 12:57:41 +0100

    Restructure and style input form

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/booksSlice';
+import './styles/input.css';
 
 const Input = () => {
   const [title, setTitle] = useState('');
@@ -32,10 +33,35 @@ const Input = () => {
   return (
     <div className="form-container">
       <h2>Add New Book</h2>
-      <form>
-        <input type="text" placeholder="Book title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Book author" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        <input type="text" placeholder="Book category" id="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+      <form className="input-form">
+        <input
+          type="text"
+          placeholder="Book title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="title"
+        />
+        <input
+          type="text"
+          placeholder="Book author"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          className="author"
+        />
+        <div className="category-input">
+          <input
+            type="text"
+            placeholder="Category"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="category"
+          />
+          <i className="fas fa-caret-down" />
+        </div>
+
         {errorMessage && <div className="text-danger error-message">{errorMessage}</div>}
         <button type="submit" id="add-new-book" onClick={submitHandler}>ADD BOOK</button>
       </form>
